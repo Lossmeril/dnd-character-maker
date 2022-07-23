5c8f3252d3e76f4fc168fd9b3a40b16dfb578d1f
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2022-07-23 19:55:39 +0200

    fix: stat calculation in the overview now accounts for race modifiers

--- components/CharacterEditor/Panes/StatPane.tsx
+++ components/CharacterEditor/Panes/StatPane.tsx
@@ -1,135 +1,157 @@
 import { EditorPane } from "./EditorPane";
 import PaneCard from "./PaneCard";
-import { FindRaceDetail, FindStatDetail } from "../../../datasets/computed/details";
+import {
+  FindRaceDetail,
+  FindStatDetail,
+} from "../../../datasets/computed/details";
 import { StatList } from "../../../datasets/computed/enumerator";
 import {
-    Box,
-    Button, Center,
-    Flex,
-    Slider,
-    SliderFilledTrack,
-    SliderThumb,
-    SliderTrack, Stack,
-    Text, Tooltip
+  Box,
+  Button,
+  Center,
+  Flex,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Stack,
+  Text,
+  Tooltip,
 } from "@chakra-ui/react";
 import { StatId } from "../../../datasets/Stats";
 import {
-    calculateDistributedPoints, calculateResultStatValue,
-    Character,
-    CHARACTER_BASE_DISTRIBUTABLE_POINTS, DEFAULT_STAT_VALUE,
-    defaultStatMap
+  calculateDistributedPoints,
+  calculateResultStatValue,
+  Character,
+  CHARACTER_BASE_DISTRIBUTABLE_POINTS,
+  DEFAULT_STAT_VALUE,
+  defaultStatMap,
 } from "../../Character";
 import Bold from "../../Bold";
 import { useState } from "react";
 import { CalculateStatModifier, GetStatPointCost } from "../../StatUtil";
 
-
 type StatSliderProps = {
-    statId: StatId,
-    character: Character,
-    onStatChange: (newValue: number) => void
-}
+  statId: StatId;
+  character: Character;
+  onStatChange: (newValue: number) => void;
+};
 
-const StatSlider = ({statId, character, onStatChange}: StatSliderProps) => {
-    const stat = FindStatDetail(statId)
-    const statValue = character.stats[statId]
+const StatSlider = ({ statId, character, onStatChange }: StatSliderProps) => {
+  const stat = FindStatDetail(statId);
+  const statValue = character.stats[statId];
 
-    const [showTooltip, setShowTooltip] = useState(false)
+  const [showTooltip, setShowTooltip] = useState(false);
 
-    const resultStatValue = calculateResultStatValue(character, statId)
+  const resultStatValue = calculateResultStatValue(character, statId);
 
-    const statModifier = CalculateStatModifier(resultStatValue)
+  const statModifier = CalculateStatModifier(resultStatValue);
 
-    return (
-        <Box>
-            <Flex justifyContent="space-between">
-                <Text>{stat.name}</Text>
-                <Text>
-                    <Bold>{resultStatValue} ({statModifier >= 0 ? `+${statModifier}` : `${statModifier}`})</Bold>
-                </Text>
-            </Flex>
-            <Slider step={1} min={8} max={15}
-                    value={statValue}
-                    colorScheme="teal"
-                    onMouseEnter={() => setShowTooltip(true)}
-                    onMouseLeave={() => setShowTooltip(false)}
-                    onChange={onStatChange}>
-                <SliderTrack>
-                    <SliderFilledTrack/>
-                </SliderTrack>
-                <SliderThumb/>
-                <Tooltip
-                    hasArrow
-                    bg="teal.500"
-                    color='white'
-                    placement='top'
-                    isOpen={showTooltip}
-                    label={`${statValue}`}
-                >
-                    <SliderThumb/>
-                </Tooltip>
-            </Slider>
-        </Box>
-    )
-}
+  return (
+    <Box>
+      <Flex justifyContent="space-between">
+        <Text>{stat.name}</Text>
+        <Text>
+          <Bold>
+            {resultStatValue} (
+            {statModifier >= 0 ? `+${statModifier}` : `${statModifier}`})
+          </Bold>
+        </Text>
+      </Flex>
+      <Slider
+        step={1}
+        min={8}
+        max={15}
+        value={statValue}
+        colorScheme="teal"
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+        onChange={onStatChange}
+      >
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+        <Tooltip
+          hasArrow
+          bg="teal.500"
+          color="white"
+          placement="top"
+          isOpen={showTooltip}
+          label={`${statValue}`}
+        >
+          <SliderThumb />
+        </Tooltip>
+      </Slider>
+    </Box>
+  );
+};
 
 const StatPane: EditorPane = ({
-                                  character, onCharacterModified,
-                                  onNavigateBack, onNavigateForward
-                              }) => {
-    const totalDistributablePoints = CHARACTER_BASE_DISTRIBUTABLE_POINTS
-    const alreadyDistributedPoints = calculateDistributedPoints(character)
+  character,
+  onCharacterModified,
+  onNavigateBack,
+  onNavigateForward,
+}) => {
+  const totalDistributablePoints = CHARACTER_BASE_DISTRIBUTABLE_POINTS;
+  const alreadyDistributedPoints = calculateDistributedPoints(character);
 
-    const resetStats = () => {
-        onCharacterModified({
-            ...character,
-            stats: defaultStatMap(DEFAULT_STAT_VALUE)
-        })
+  const resetStats = () => {
+    onCharacterModified({
+      ...character,
+      stats: defaultStatMap(DEFAULT_STAT_VALUE),
+    });
+  };
+
+  const changeStat = (statId: StatId) => (newValue: number) => {
+    const valueDelta = newValue - character.stats[statId];
+
+    // only validate if user adds points to a stat
+    if (valueDelta > 0) {
+      const newDistributedPoints =
+        alreadyDistributedPoints +
+        GetStatPointCost(newValue) -
+        GetStatPointCost(character.stats[statId]);
+
+      if (newDistributedPoints > totalDistributablePoints) {
+        return;
+      }
     }
 
-    const changeStat = (statId: StatId) => (newValue: number) => {
-        const valueDelta = newValue - character.stats[statId]
+    onCharacterModified({
+      ...character,
+      stats: {
+        ...character.stats,
+        [statId]: newValue,
+      },
+    });
+  };
 
-        // only validate if user adds points to a stat
-        if(valueDelta > 0) {
-            const newDistributedPoints =
-                alreadyDistributedPoints
-                + GetStatPointCost(newValue)
-                - GetStatPointCost(character.stats[statId])
+  return (
+    <PaneCard
+      title="Stats"
+      onNavigateBack={onNavigateBack}
+      onNavigateForward={onNavigateForward}
+    >
+      {StatList.map((statId) => (
+        <StatSlider
+          key={statId}
+          character={character}
+          statId={statId}
+          onStatChange={changeStat(statId)}
+        ></StatSlider>
+      ))}
+      <Flex justifyContent="space-between" mt={4}>
+        <Text>
+          Zbývající body:
+          <Bold> {totalDistributablePoints - alreadyDistributedPoints}</Bold>
+        </Text>
+        <Button onClick={resetStats} colorScheme="red">
+          Reset
+        </Button>
+      </Flex>
+    </PaneCard>
+  );
+};
 
-            if(newDistributedPoints > totalDistributablePoints) {
-                return;
-            }
-        }
-
-        onCharacterModified({
-            ...character,
-            stats: {
-                ...character.stats,
-                [statId]: newValue
-            },
-        })
-    }
-
-    return (
-        <PaneCard title="Stats"
-                  onNavigateBack={onNavigateBack} onNavigateForward={onNavigateForward}>
-            {StatList.map((statId) =>
-                (<StatSlider
-                    key={statId}
-                    character={character}
-                    statId={statId}
-                    onStatChange={changeStat(statId)}
-                ></StatSlider>)
-            )}
-            <Flex justifyContent="space-between" mt={4}>
-                <Text>Zbývající body:
-                    <Bold> {totalDistributablePoints - alreadyDistributedPoints}</Bold>
-                </Text>
-                <Button onClick={resetStats} colorScheme="red">Reset</Button>
-            </Flex>
-        </PaneCard>
-    )
-}
-
-export default StatPane
+export default StatPane;
--- components/CharacterOverview.tsx
+++ components/CharacterOverview.tsx
@@ -1,90 +1,97 @@
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import {
-    FindClassDetail,
-    FindRaceDetail,
-    FindSexDetail,
-    FindSkillDetail, FindSpecialAbilityDetail,
-    FindStatDetail
+  FindClassDetail,
+  FindRaceDetail,
+  FindSexDetail,
+  FindSkillDetail,
+  FindSpecialAbilityDetail,
+  FindStatDetail,
 } from "../datasets/computed/details";
 import _ from "lodash";
 import { ClassId } from "../datasets/Classes";
 import { StatId } from "../datasets/Stats";
-import { Character } from "./Character";
+import { calculateResultStatValue, Character } from "./Character";
+import { CalculateStatModifier } from "./StatUtil";
 
 type OverviewFieldProps = {
-    label: string,
-    children: React.ReactNode
-}
+  label: string;
+  children: React.ReactNode;
+};
 
-const OverviewField = ({label, children}: OverviewFieldProps) => (
-    <Box mt={2}>
-        <Text size="larger" fontWeight="bold">{label}</Text>
-        <Box>{children}</Box>
-    </Box>
-)
+const OverviewField = ({ label, children }: OverviewFieldProps) => (
+  <Box mt={2}>
+    <Text size="larger" fontWeight="bold">
+      {label}
+    </Text>
+    <Box>{children}</Box>
+  </Box>
+);
 
 type CharacterOverviewProps = {
-    character: Character
-}
+  character: Character;
+};
 
-const CharacterOverview = ({character}: CharacterOverviewProps) => {
-    return (
-        <Box
-            //@ts-ignore
-            display={{base: null, md: "grid"}}
-            //@ts-ignore
-            gridTemplateColumns={{base: null, md: "1fr 1fr"}}
-        >
-            <OverviewField label="Jméno">{character.name}</OverviewField>
-            <OverviewField label="Pohlaví">{FindSexDetail(character.sex).name}</OverviewField>
-            <OverviewField label="Rasa">{FindRaceDetail(character.race).name}</OverviewField>
-            <OverviewField label="Úroveň">{character.level}</OverviewField>
-            <OverviewField label="Povolání">
-                {
-                    _(character.classes)
-                        .entries()
-                        .filter(([_, level]) => level > 0)
-                        .map(([classId, level]) => (
-                            <Text key={classId}>
-                                {FindClassDetail(classId as unknown as ClassId).name} (úroveň {level})
-                            </Text>
-                        )).toJSON()
-                }
-            </OverviewField>
-            <OverviewField label="Staty">
-                {
-                    _(character.stats)
-                        .entries()
-                        .map(([statId, level]) => (
-                            <Text key={statId}>
-                                {FindStatDetail(statId as unknown as StatId).name}: {level}
-                            </Text>
-                        )).value()
-                }
-            </OverviewField>
-            <OverviewField label="Proficiencies">
-                {
-                    character
-                        .proficiencies
-                        .map(FindSkillDetail)
-                        .map((skill, i) => (
-                            <Text key={i}>{skill.name}</Text>
-                        ))
-                }
-            </OverviewField>
-            <OverviewField label="Special Ability">
-                {
-                    character
-                        .specialAbilities
-                        .map(FindSpecialAbilityDetail)
-                        .map((specialAbility, i) => (
-                            <Text key={i}>{specialAbility.name}</Text>
-                        ))
-                }
-            </OverviewField>
-        </Box>
-    )
-}
+const CharacterOverview = ({ character }: CharacterOverviewProps) => {
+  return (
+    <Box
+      //@ts-ignore
+      display={{ base: null, md: "grid" }}
+      //@ts-ignore
+      gridTemplateColumns={{ base: null, md: "1fr 1fr" }}
+    >
+      <OverviewField label="Jméno">{character.name}</OverviewField>
+      <OverviewField label="Pohlaví">
+        {FindSexDetail(character.sex).name}
+      </OverviewField>
+      <OverviewField label="Rasa">
+        {FindRaceDetail(character.race).name}
+      </OverviewField>
+      <OverviewField label="Úroveň">{character.level}</OverviewField>
+      <OverviewField label="Povolání">
+        {_(character.classes)
+          .entries()
+          .filter(([_, level]) => level > 0)
+          .map(([classId, level]) => (
+            <Text key={classId}>
+              {FindClassDetail(classId as unknown as ClassId).name} (úroveň{" "}
+              {level})
+            </Text>
+          ))
+          .toJSON()}
+      </OverviewField>
+      <OverviewField label="Staty">
+        {_(character.stats)
+          .entries()
+          .map(([statId, level]) => (
+            <Text key={statId}>
+              {FindStatDetail(statId as unknown as StatId).name}:{" "}
+              {calculateResultStatValue(character, statId as unknown as StatId)}{" "}
+              (
+              {CalculateStatModifier(
+                calculateResultStatValue(character, statId as unknown as StatId)
+              )}
+              )
+            </Text>
+          ))
+          .value()}
+      </OverviewField>
+      <OverviewField label="Proficiencies">
+        {character.proficiencies.map(FindSkillDetail).map((skill, i) => (
+          <Text key={i}>{skill.name}</Text>
+        ))}
+      </OverviewField>
+      <OverviewField label="Special Ability">
+        {character.specialAbilities
+          .map(FindSpecialAbilityDetail)
+          .map((specialAbility, i) => (
+            <Text key={i}>{specialAbility.name}</Text>
+          ))}
+      </OverviewField>
 
-export default CharacterOverview
+      <OverviewField label="Magie">{}</OverviewField>
+    </Box>
+  );
+};
+
+export default CharacterOverview;
